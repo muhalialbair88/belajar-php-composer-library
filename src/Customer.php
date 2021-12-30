52516f366688b82ea9b75c3a92ebd1fef4f70ae4
muhalialbair88 <muhalialbair88@gmail.com>
2021-12-30 10:00:35 +0700

    initial-commit

---
+++ src/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Alialbair\Belajar;
+
+class Customer{
+        public function __construct(private string $name)
+        {
+            
+        }
+
+        public function sayHello(string $name = "Guest"):string{
+            return "Hello $name, My name is $this->name";
+        }
+}
+?>
